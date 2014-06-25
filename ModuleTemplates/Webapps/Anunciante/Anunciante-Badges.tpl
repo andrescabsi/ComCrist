b324e6710e0f44c81ea302fb1e8aaa1389e90cdb
Andres Cabrera <andres@innov.com.co>
2014-06-24 23:38:35 -0500

    Cambios varios

---
+++ ModuleTemplates/Webapps/Anunciante/Anunciante-Badges.tpl
@@ -0,0 +1,26 @@
+
+<script>
+
+$("document").ready(function(){
+
+	if ({tag_cieec}) {
+		console.log("Verdadero SIEEC");
+		$("#badge_cieec_img").show();
+	};
+
+	if ({tag_empresario biblico}) {
+		console.log("Verdadero Diezmador");
+		$("#badge_diezmadorleal_img").show();
+	};
+
+});
+
+
+</script>
+
+<a id="badge_diezmadorleal_img" style="display:none;" href="/Comercio-Cristiano/Diezmador-leal.html">
+	<img src="/Imagenes/Badges/DiezmadorLeal_Badge.png?Action=thumbnail&Width=130&Height=130&algorithm=fill_proportional" alt="Esta empresa hace parte de Diezmador Leal">
+</a>
+<a id="badge_cieec_img" style="display:none;" href="/Comercio-Cristiano/SIEEC.html">
+	<img src="/Imagenes/Badges/SIEEC_Badge.png?Action=thumbnail&Width=130&Height=130&algorithm=fill_proportional" alt="Esta empresa está respaldada por SIEEC">
+</a>
--- js/Publicaciones/CamposCategorias.js
+++ js/Publicaciones/CamposCategorias.js
@@ -9,14 +9,68 @@ $(document).ready(function() {
             itemsvacios.parent().remove();
         }
     });
-    
-    
+
+    // Calculo Ahorro
+    //************************************
+    mostrar_Ahorro_precio();
+
+    // Enlace Categoria
+    //************************************
+    enlace_categoriaPublicacionActual();
 
 
         
 });
 
 
+// Enlace categoria breadcrumbs
+//************************************
+function enlace_categoriaPublicacionActual(){
+    var categoriaPublicacionActual = $("#categoria_Publi_enlace").html();
+
+    if (categoriaPublicacionActual == "Aplicaciones") {
+        $("#categoria_Publi_enlace").attr("href","/categoria/aplicaciones");
+    }else if (categoriaPublicacionActual == "Bienes-Raices") {
+        $("#categoria_Publi_enlace").attr("href","/categoria/bienes-raices");
+    }else if (categoriaPublicacionActual == "Compra-Venta") {
+        $("#categoria_Publi_enlace").attr("href","/categoria/compra-venta");
+    }else if (categoriaPublicacionActual == "Viajes-Turismo") {
+        $("#categoria_Publi_enlace").attr("href","/categoria/viajes-turismo");
+    }else if (categoriaPublicacionActual == "Empleo") {
+        $("#categoria_Publi_enlace").attr("href","/categoria/empleo");
+    }else if (categoriaPublicacionActual == "Clases-Cursos") {
+        $("#categoria_Publi_enlace").attr("href","/categoria/clases-cursos");
+    }else if (categoriaPublicacionActual == "Vehiculos") {
+        $("#categoria_Publi_enlace").attr("href","/categoria/vehiculos");
+    }else if (categoriaPublicacionActual == "Servicios") {
+        $("#categoria_Publi_enlace").attr("href","/categoria/servicios");
+    }else if (categoriaPublicacionActual == "Emisoras") {
+        $("#categoria_Publi_enlace").attr("href","/categoria/emisoras");
+    }else if (categoriaPublicacionActual == "Periódicos y Revistas") {
+        $("#categoria_Publi_enlace").attr("href","periodicos-y-revistas");
+    }else if (categoriaPublicacionActual == "Comunidad") {
+        $("#categoria_Publi_enlace").attr("href","/categoria/comunidad");
+    }else if (categoriaPublicacionActual == "Oportunidades de Negocio") {
+        $("#categoria_Publi_enlace").attr("href","/categoria/oportunidades-de-negocio");
+    }else{
+        $("#categoria_Publi_enlace").attr("href","/");
+    }
+};
+
+
+
+
+// Calculo Ahorro
+//************************************
+function mostrar_Ahorro_precio(){
+
+    var variablePrecioAhora = $("#variablePrecioAhora").html();
+    var variablePrecioAntes = $("#variablePrecioAntes").html();
+    var variablePrecioAhorro = variablePrecioAntes - variablePrecioAhora;
+
+    //console.log(variablePrecioAhorro);
+    $("#variablePrecioAhorro").html(variablePrecioAhorro);
+};
     
     
 function mostrar_Campos_Pubs(){
